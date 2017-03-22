c1a10600cd953285e0b145f53e0503a9c9206660
Christian Rivera <chris.arevir@gmail.com>
2017-03-22 16:29:45 -0700

    working hover button thing

--- navigation/header/src/Header.vue
+++ navigation/header/src/Header.vue
@@ -1,19 +1,10 @@
 <template>
   <div id="header-template" class="header-wrapper">
-    {{ greeting }}
+    <button class="back-button"><i class="fa fa-angle-left back-icon"></i></button>
   </div>
 </template>
 
-<script>
-
-module.exports = {
-  name: 'header',
-  data: () => ({ greeting: 'Hello world!' })
-}
-
-</script>
-
-<style scoped>
+<style scoped lang="scss">
 * {
   box-sizing: border-box;
   cursor: default;
@@ -21,10 +12,40 @@ module.exports = {
   padding: 0;
 }
 
-.header-wrapper {
-  display: flex;
-  background: black;
+.back-button {
+  background: darkturquoise;
+  border: none;
   color: white;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  transition: background 0.3s ease;
+  width: 32px;
+
+  &:hover {
+    background: darkorange;
+  }
+}
+
+
+.back-icon {
+  font-size: 2.5em;
+}
+
+.header-wrapper {
+  background: black;
+  display: flex;
+  flex-direction: row;
+  height: 65px;
   width: 100%;
 }
 </style>
+
+<script>
+
+module.exports = {
+  name: 'header',
+  data: () => ({ hoverOnBack: false })
+}
+
+</script>
